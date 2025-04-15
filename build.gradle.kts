33ec349173a1360055b9a04873a70e572680f4ed
yy <1311095683@qq.com>
2025-04-15 22:59:10 +0800

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+import com.github.gradle.node.npm.task.NpmTask
+
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.24"
+    id("org.jetbrains.intellij") version "1.17.3"
+    id("com.github.node-gradle.node") version "7.1.0"
+}
+
+group = "org.arkpermission"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2023.2.6")
+    type.set("IC") // Target IDE Platform
+
+    plugins.set(listOf(/* Plugin Dependencies */))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("232")
+        untilBuild.set("242.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+
+    // 构建ts文件
+    register<NpmTask>("compileTypeScript") {
+        args.set(listOf("run", "build"))
+    }
+
+    // 添加与 build 任务的依赖关系
+    named("build") {
+        dependsOn("compileTypeScript")
+    }
+
+    named("clean") {
+        doLast {
+            delete("build/resources/main")
+        }
+    }
+}
